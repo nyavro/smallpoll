3e8bb0a8583451093650a8c64158b5bd96ce716f
Nyavro <e.nyavro@gmail.com>
2015-04-24 23:33:04 +0600

    Initial commit

---
+++ src/main/scala/samples/employeedirectory/EmployeeList.scala
@@ -0,0 +1,12 @@
+package samples.employeedirectory
+
+import android.app.Activity
+import android.os.Bundle
+
+class EmployeeList extends Activity {
+
+  override def onCreate(savedInstanceState:Bundle) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.main)
+  }
+}
