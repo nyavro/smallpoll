3cc7e1d3ead00a16ad54470652c4b51635fc52c0
Nyavro <e.nyavro@gmail.com>
2015-05-12 15:41:50 +0600

    Multi option provided

--- src/main/scala/com/eny/smallpoll/model/Question.scala
+++ src/main/scala/com/eny/smallpoll/model/Question.scala
@@ -3,6 +3,6 @@ package com.eny.smallpoll.model
 /**
  * Created by eny on 25.04.15.
  */
-case class Question(id:Option[Long], text:String) {
+case class Question(id:Option[Long], text:String, multi:Boolean) {
   override def toString = text
 }
--- src/main/scala/com/eny/smallpoll/repository/QuestionRepositoryImpl.scala
+++ src/main/scala/com/eny/smallpoll/repository/QuestionRepositoryImpl.scala
@@ -10,19 +10,19 @@ import com.eny.smallpoll.model.Question
 class QuestionRepositoryImpl(db:SQLiteDatabase) extends QuestionRepository with CursorConversion {
 
   override def load(id: Long): Question = {
-    val cursor = db.rawQuery(s"SELECT id, txt FROM question WHERE id=?", Array(id.toString))
+    val cursor = db.rawQuery(s"SELECT _id, txt FROM question WHERE id=?", Array(id.toString))
     cursor.moveToFirst
     convert(cursor)
   }
 
   override def list(surveyId: Long): List[Question] = {
-    val cursor = db.rawQuery("SELECT q._id, q.txt FROM question q INNER JOIN survey_question sq ON q._id=sq.question_id WHERE sq.survey_id=?", Array(surveyId.toString))
+    val cursor = db.rawQuery("SELECT q._id, q.txt, q.multi FROM question q INNER JOIN survey_question sq ON q._id=sq.question_id WHERE sq.survey_id=?", Array(surveyId.toString))
     cursor.moveToFirst
     toList(cursor, convert)
   }
 
   override def save(question: Question): Unit = {
-    val values = Values(Map("txt"->question.text)).content
+    val values = Values(Map("txt"->question.text, "multi"->question.multi)).content
     question.id match {
       case Some(id) =>
         db.update("question", values, "_id=?", Array(id.toString))
@@ -31,5 +31,5 @@ class QuestionRepositoryImpl(db:SQLiteDatabase) extends QuestionRepository with 
     }
   }
 
-  def convert(cursor:Cursor) = Question(Some(cursor.getLong(0)), cursor.getString(1))
+  def convert(cursor:Cursor) = Question(Some(cursor.getLong(0)), cursor.getString(1), cursor.getInt(2)==1)
 }
