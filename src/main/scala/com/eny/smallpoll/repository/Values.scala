26536b178ee4b04fd7556d54e333375c11f3d8dd
Nyavro <e.nyavro@gmail.com>
2015-05-15 01:12:27 +0600

    Survey run full cycle

--- src/main/scala/com/eny/smallpoll/repository/Values.scala
+++ src/main/scala/com/eny/smallpoll/repository/Values.scala
@@ -1,6 +1,7 @@
 package com.eny.smallpoll.repository
 
 import java.lang
+import java.util.Date
 
 import android.content.ContentValues
 
@@ -15,6 +16,7 @@ case class Values(map:Map[String,Any]) {
       case (key, value:Int) => res.put(key, new Integer(value))
       case (key, value:Long) => res.put(key, new lang.Long(value))
       case (key, value:Boolean) => res.put(key, new lang.Boolean(value))
+      case (key, value:Date) => res.put(key, new lang.Long(value.getTime))
     }
     res
   }
--- src/main/scala/com/eny/smallpoll/view/SurveyRunView.scala
+++ src/main/scala/com/eny/smallpoll/view/SurveyRunView.scala
@@ -2,10 +2,11 @@ package com.eny.smallpoll.view
 
 import java.util.Date
 
+import android.graphics.Color
 import android.os.Bundle
 import android.util.SparseBooleanArray
 import android.view.View
-import android.widget.AbsListView
+import android.widget.{AdapterView, AbsListView}
 import com.eny.smallpoll.R
 import com.eny.smallpoll.model.{Answer, Result}
 import com.eny.smallpoll.repository.{AnswerRepository, QuestionRepository, ResultRepository}
@@ -22,51 +23,82 @@ class SurveyRunView extends SActivity with Db {
   lazy val multiChoice = new SListView
   lazy val singleChoice = new SListView
   lazy val next = new SButton
+  lazy val thanks = new STextView
+  lazy val layout = new SVerticalLayout
   var questionIds = Array[Long]()
-  onCreate {
-    questionIds = questionRepository.list(getIntent.getLongExtra("surveyId", -1)).map(_.id.get).toArray
-    if(questionIds.isEmpty) {
-    } else {
-      next.setText(R.string.next)
-      next.onClick {
-        def selectedIds(i:Int, ids:SparseBooleanArray):List[Long] = {
-          if(i<ids.size) {
-            if(ids.valueAt(i)) {
-              val orElse: Long = multiChoice.getAdapter.getItem(i).asInstanceOf[Answer].id.getOrElse(-1L)
-              orElse::selectedIds(i+1, ids)
-            } else {
-              selectedIds(i+1, ids)
-            }
-          } else {
-            Nil
-          }
-        }
-        selectedIds(0, multiChoice.getCheckedItemPositions).map {
-          answerId => resultRepository.save(Result(new Date, answerId))
-        }
-        questionIds = questionIds.tail
-        update
-      }
-      multiChoice.setChoiceMode(AbsListView.CHOICE_MODE_MULTIPLE)
-      contentView(
-        new SVerticalLayout += text += multiChoice += singleChoice += next
-      )
-    }
+  var surveyId = -1L
+
+  def initArguments() = {
+    surveyId = getIntent.getLongExtra("surveyId", -1L)
+    questionIds = questionRepository.list(surveyId).map(_.id.get).toArray
   }
-  def update() = {
-    val question = questionRepository.load(questionIds(0))
-    val answers = answerRepository.list(question.id.get)
-    text.setText(question.text)
-    if(question.multi) {
-      multiChoice.setAdapter(new SArrayAdapter(answers.toArray, android.R.layout.simple_list_item_multiple_choice))
-      multiChoice.setVisibility(View.VISIBLE)
-      singleChoice.setVisibility(View.GONE)
-      next.setVisibility(View.VISIBLE)
-    } else {
-      singleChoice.setAdapter(new SArrayAdapter(answers.toArray))
+
+  onCreate {
+    initArguments()
+    next.setText(R.string.next)
+    next.onClick {
+      def selectedIds(i:Int, ids:SparseBooleanArray):List[Long] = {
+        if(i<ids.size) {
+          if(ids.valueAt(i)) {
+            val orElse: Long = multiChoice.getAdapter.getItem(i).asInstanceOf[Answer].id.getOrElse(-1L)
+            orElse::selectedIds(i+1, ids)
+          } else {
+            selectedIds(i+1, ids)
+          }
+        } else {
+          Nil
+        }
+      }
+      selectedIds(0, multiChoice.getCheckedItemPositions).map {
+        answerId => resultRepository.save(Result(new Date, answerId))
+      }
+      questionIds = questionIds.tail
+      update()
+    }
+    singleChoice.onItemClick {
+      (adapterView:AdapterView[_], view:View, position:Int, id:Long) =>
+        val answerId = singleChoice.getAdapter.getItem(position).asInstanceOf[Answer].id.getOrElse(-1L)
+        resultRepository.save(Result(new Date, answerId))
+        questionIds = questionIds.tail
+        update()
+    }
+    multiChoice.setChoiceMode(AbsListView.CHOICE_MODE_MULTIPLE)
+    thanks.setText(R.string.thanks)
+    layout.onClick {
+      if(questionIds.isEmpty) {
+        initArguments()
+        update()
+      }
+    }
+    contentView(
+      layout += text += multiChoice += singleChoice += next += thanks
+    )
+  }
+  def update():Unit = {
+    if(questionIds.isEmpty) {
+      thanks.setVisibility(View.VISIBLE)
       multiChoice.setVisibility(View.GONE)
-      singleChoice.setVisibility(View.VISIBLE)
+      singleChoice.setVisibility(View.GONE)
       next.setVisibility(View.GONE)
+      text.setVisibility(View.GONE)
+    }
+    else {
+      val question = questionRepository.load(questionIds.head)
+      val answers = answerRepository.list(question.id.get)
+      text.setText(question.text)
+      if(question.multi) {
+        multiChoice.setAdapter(new SArrayAdapter(answers.toArray, android.R.layout.simple_list_item_multiple_choice))
+        multiChoice.setVisibility(View.VISIBLE)
+        singleChoice.setVisibility(View.GONE)
+        next.setVisibility(View.VISIBLE)
+      } else {
+        singleChoice.setAdapter(new SArrayAdapter(answers.toArray))
+        multiChoice.setVisibility(View.GONE)
+        singleChoice.setVisibility(View.VISIBLE)
+        next.setVisibility(View.GONE)
+      }
+      text.setVisibility(View.VISIBLE)
+      thanks.setVisibility(View.GONE)
     }
   }
   onResume {
@@ -74,10 +106,12 @@ class SurveyRunView extends SActivity with Db {
   }
   override def onSaveInstanceState(bundle:Bundle) = {
     super.onSaveInstanceState(bundle)
+    bundle.putLong("suveyId", surveyId)
     bundle.putLongArray("questionIds", questionIds)
   }
   override def onRestoreInstanceState(bundle:Bundle) = {
     super.onRestoreInstanceState(bundle)
+    surveyId = bundle.getLong("surveyId")
     questionIds = Array(bundle.getLong("questionIds"))
   }
 }
